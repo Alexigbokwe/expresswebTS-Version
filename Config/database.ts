c3879eb960dd890c66c5d01043f4ac663f7c5f18
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-12 13:41:03 +0100

    cleanup: database
    Migration and Seed clean up

--- Config/database.ts
+++ Config/database.ts
@@ -25,7 +25,7 @@ export default {
       directory: path.join(__dirname, '../Database/Migrations/'),
       tableName: "migrations",
       stub:  path.join(__dirname, '../Database/Migrations/migrationLayout.stub'),
-      extension: 'ts'
+      extension: 'js'
     },
     seeds: {
       directory:  path.join(__dirname, '../Database/Seeds/'),
@@ -56,7 +56,7 @@ export default {
       directory: path.join(__dirname, '../Database/Migrations/'),
       tableName: "migrations",
       stub:  path.join(__dirname, '../Database/Migrations/migrationLayout.stub'),
-      extension: 'ts'
+      extension: 'js'
     },
     seeds: {
       directory:  path.join(__dirname, '../Database/Seeds/'),
