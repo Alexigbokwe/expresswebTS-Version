2e5a7233aca144575ebbfbcd607eb663db06c89a
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-21 02:51:32 +0100

    Delete build directory

--- build/App/Listeners/Book_listener.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Book {
-    /**
-     * Handle the event.
-     * @param {*} eventName
-     * @param {*} params
-     */
-    constructor(eventName: string, params: any);
-}
-export default Book;
--- build/Config/cors.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    origin: boolean;
-    methods: string[];
-    headers: boolean;
-    exposeHeaders: boolean;
-    credentials: boolean;
-    maxAge: number;
-};
-export default _default;
