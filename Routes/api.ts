852d5b543d06b96ace0413692bbab7cbe0836e1e
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-24 09:51:19 +0100

    feature: jobs and queue worker integration

--- Routes/api.ts
+++ Routes/api.ts
@@ -14,5 +14,7 @@ Route.get("/", (req: Request, res: Response) => {
   res.status(200).send("Welcome to ExpressWebJs Typescript Version");
 });
 
+Route.get("/users", "UserController@index");
+
 //--------------------------------------------------------------------------
 export default Route.exec;
