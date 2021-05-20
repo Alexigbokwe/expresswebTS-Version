a17f241c5f23fb1fcd88a88551d277fb3681a1e8
Alex Igbokwe <juvitafrica@gmail.com>
2021-05-20 12:55:47 +0100

    fix:  request validation
    Change request validation to request validate

--- App/Http/Middleware/RequestBodyValidator.ts
+++ App/Http/Middleware/RequestBodyValidator.ts
@@ -4,7 +4,9 @@ import { Request, Response, NextFunction } from "Elucidate/HttpContext";
 
 class RequestBodyValidator {
   async handle(req: Request, res: Response, next: NextFunction) {
-    req.validation = FormRequest;
+    req.validate = (data: object, rules: object) => {
+      return FormRequest.make(data, rules);
+    };
 
     await next();
   }
